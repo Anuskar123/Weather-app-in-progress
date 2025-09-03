619e7d521ce4a28c7f03d7ca734a358f57c838ff
Anuskar Sigdel <anuskarsigdel04@gmail.com>
2025-09-03 19:10:11 +0545

    Update App.jsx to integrate WeatherBoard and providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import WeatherBoard from "./components/weather/WeatherBoard";
+import { FavouriteProvider, WeatherProvider } from "./provider";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-    </div>
-  )
+    <WeatherProvider>
+      <FavouriteProvider>
+        <div className='bg-body font-[Roboto] text-light bg-no-repeat bg-cover grid place-items-center'>
+          <WeatherBoard />
+        </div>
+      </FavouriteProvider>
+    </WeatherProvider>
+  );
 }
 
-export default App
+export default App;
